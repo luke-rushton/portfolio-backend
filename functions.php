22022e6ce99e7336ca5cfb3636d0949252ebb50f
Luke Rushton <luke.rushtonx@gmail.com>
2024-10-02 15:01:17 -0700

    added works CPT

--- functions.php
+++ functions.php
@@ -204,3 +204,59 @@ if ( ! function_exists( 'twentytwentyfour_pattern_categories' ) ) :
 endif;
 
 add_action( 'init', 'twentytwentyfour_pattern_categories' );
+
+// Register Custom Post Type
+function custom_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Portfolio Works', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Portfolio Work', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Portfolio Works', 'text_domain' ),
+		'name_admin_bar'        => __( 'Portfolio Work', 'text_domain' ),
+		'archives'              => __( 'Works Archives', 'text_domain' ),
+		'attributes'            => __( 'Work Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Works', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Work', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Work', 'text_domain' ),
+		'edit_item'             => __( 'Edit Work', 'text_domain' ),
+		'update_item'           => __( 'Update Work', 'text_domain' ),
+		'view_item'             => __( 'View Work', 'text_domain' ),
+		'view_items'            => __( 'View Works', 'text_domain' ),
+		'search_items'          => __( 'Search Work', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into work', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this work', 'text_domain' ),
+		'items_list'            => __( 'workss list', 'text_domain' ),
+		'items_list_navigation' => __( 'Works list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter works list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Portfolio Work', 'text_domain' ),
+		'description'           => __( 'portfolio works', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'portfolio_work', $args );
+
+}
+add_action( 'init', 'custom_post_type', 0 );
